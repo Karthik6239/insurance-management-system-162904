275ff1fe953f6a39c83d57ae4dbb49c345742e84
Kavia AI <production@kavia.ai>
2025-08-22 16:55:06 +0000

    CheckPoint - cg1dcb949b

---
+++ insurance_management_backend/src/main/java/com/example/demo/insurancemanagementbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancemanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class insurancemanagementbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(insurancemanagementbackendApplication.class, args);
+	}
+
+}
---
+++ insurance_management_backend/src/test/java/com/example/demo/insurancemanagementbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurancemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class insurancemanagementbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
